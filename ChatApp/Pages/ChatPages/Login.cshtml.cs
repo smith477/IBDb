8b92bbeec8c1561eacaa90b28d227b5db648bd00
marezii <marko.m.ilic@outlook.com>
2020-04-20 18:40:38 +0200

    ChatApp Frontend Setup

---
+++ ChatApp/Pages/ChatPages/Login.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace ChatApp.Pages.ChatPages
+{
+    public class LoginModel : PageModel
+    {
+
+    }
+}
---
+++ ChatApp/wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+
+var username = document.getElementById("Username");
+var password = document.getElementById("Password");
+
+var loginButton = document.getElementById("LoginButton");
+
+$(loginButton).click(function () {
+    $.ajax({
+        url: "/api/user/login",
+        type: "POST",
+        data: {
+            "username": username.value,
+            "password": password.value
+        },
+        contentType: "application/json",
+        dataType: "json",
+        success: function (response) {
+            console.log(response);
+        }
+    })
+})
+
